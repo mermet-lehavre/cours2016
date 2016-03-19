9bc9f06012bbc87b66d90b0eff2461b2369f6bdf
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-03-19 17:50:58 +0100

    Insertion d'une fonction

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
+	
+	public static void milieu() {
+		System.out.println("Ligne du milieu");
+	}
+	
 	public static void main( String[] args ) {
 		System.out.println("Bonjour tout le monde");
-		System.out.println("Ligne du milieu");
+		milieu();
 		System.out.println("Au revoir tout le monde");
 	}
 }
