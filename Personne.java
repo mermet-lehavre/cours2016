148d79f0f1e20fe371d75afffcaa019c0cae338e
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-03-19 17:48:54 +0100

    Ajout d'un constructeur à la classe Personne

--- Personne.java
+++ Personne.java
@@ -1,5 +1,11 @@
 public class Personne {
 	private String nom;
 	private String prenom;
-	private int age;	
+	private int age;
+	
+	public Personne(String n, String p, int a) {
+		nom = n;
+		prenom = p;
+		age = a;
+	}
 }
