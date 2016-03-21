f89b5f481af2f16f3b776dbe52db7e0f2751eb81
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-03-21 11:45:20 +0100

    Ajout d'un println

--- Personne.java
+++ Personne.java
@@ -4,6 +4,7 @@ public class Personne {
 	private int age;
 	
 	public Personne(String n, String p, int a) {
+		System.out.println("Ajout dans le projet initial");
 		nom = n;
 		prenom = p;
 		age = a;
